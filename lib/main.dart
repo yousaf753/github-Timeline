535e2e10bbe26d1caa4ea7420acbc8e4cd586304
M Yousaf <72139490+yousaf753@users.noreply.github.com>
2021-03-01 16:29:20 +0500

    TimeLine

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:github_flutter/providers/repo_provider.dart';
+import 'package:github_flutter/providers/user_provider.dart';
+import 'package:github_flutter/screen/home_screen.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(value: UserProvider()),
+        ChangeNotifierProvider.value(value: RepositriesProvider()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: HomeScreen(),
+      ),
+    );
+  }
+}
---
+++ lib/utils/api.dart
@@ -0,0 +1,4 @@
+class Api {
+  static const api = 'https://api.github.com';
+  static const token = '76727d3c27642349d32fd98e7a5fdfdb4bba8111';
+}
